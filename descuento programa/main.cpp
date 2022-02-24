b37a219336f11d95a19fb7c8c2ea0932d8af26c0
MadellynC04 <98790300+MadellynC04@users.noreply.github.com>
2022-02-23 20:11:11 -0600

    Programas

---
+++ descuento programa/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+
+ using namespace std;
+	double subtotal;
+    double total;
+    double impuesto = 0.15;
+    int descuento;
+    double calculoDescuento;
+    double calculoImpuesto;
+
+    cout << "Ingrese el subtotal de la factura:";
+    cin >> subtotal;
+    
+    cout << "Ingrese el descuento (0, 10, 15, 20): ";
+    cin >> descuento;
+    
+    calculoDescuento = (subtotal * descuento) / 100;
+    calculoImpuesto = (subtotal - calculoDescuento) * 0.15;
+    total = subtotal - calculoDescuento + calculoImpuesto;
+
+    cout << endl;
+    cout << "Total a pagar es: " << total;
+    
+
+
+	return 0;
+}
